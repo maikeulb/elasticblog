db82392b1b23c75669b46fbd7bad2064c28c785d
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-03-15 16:05:45 -0400

    add timestamp to post model

--- src/app/models/post.py
+++ src/app/models/post.py
@@ -47,6 +47,7 @@ class Post(SearchableMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
 
 db.event.listen(db.session, 'before_commit', Post.before_commit)
