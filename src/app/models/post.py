e588d84066d7cc5f4f932b8457907b91f7d6fd34
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-03-15 17:21:00 -0400

    add tag model

--- src/app/models/post.py
+++ src/app/models/post.py
@@ -4,14 +4,27 @@ from time import time
 from flask import current_app
 from app.mixins import SearchableMixin
 
+tag_posts = db.Table(
+    'tag_posts',
+    db.Column('tag_id', db.Integer, db.ForeignKey('tags.id')),
+    db.Column('post_id', db.Integer, db.ForeignKey('posts.id'))
+)
+
 
 class Post(SearchableMixin, db.Model):
     __tablename__ = 'posts'
-    __searchable__ = ['body']
+    __searchable__ = ['body', 'category']
+
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    title = db.Column(db.String(45))
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    tags = db.relationship(
+        "Tag",
+        backref='post',
+    )
 
 
 db.event.listen(db.session, 'before_commit', Post.before_commit)
---
+++ src/app/models/tag.py
@@ -0,0 +1,8 @@
+from models import db
+
+
+class Tag(db.Model):
+    __tablename__ = 'tags'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(45))
