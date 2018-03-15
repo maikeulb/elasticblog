db60ace6b0355bee70c6c2b772c4c35e520e6429
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-03-15 17:02:31 -0400

    get app working

--- src/config.py
+++ src/config.py
@@ -5,9 +5,9 @@ class Config(object):
 
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'S3cr3t'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or \
-        'postgresql://postgres:P@ssw0rd!@postgres/elasticblog'
+        'postgresql://postgres:P@ssw0rd!@172.17.0.2/elasticblog'
     ELASTICSEARCH_URL = os.environ.get('ELASTIC_URI') or \
-        'http://elastic'
+        'http://172.17.0.3'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     POSTS_PER_PAGE = 10
